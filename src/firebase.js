ecf3443dad91cc560993c135e491f6e9d683f63a
Utku İpek <73702905+ujgu@users.noreply.github.com>
2021-10-06 01:11:55 +0300

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFoBlhQWu7xOy8I3bW34ZMdGP25AxCtpA",
+  authDomain: "whatsapp-clone-a4cf8.firebaseapp.com",
+  databaseURL:
+    "https://whatsapp-clone-a4cf8-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "whatsapp-clone-a4cf8",
+  storageBucket: "whatsapp-clone-a4cf8.appspot.com",
+  messagingSenderId: "73297256686",
+  appId: "1:73297256686:web:36e1881f8a58530309d3ff",
+  measurementId: "G-EPYJ0L5XFW",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
